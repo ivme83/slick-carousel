08dd03c2adf955da997ee41a74a8c45bea55350f
Roland Willis <rolandwillisiv@gmail.com>
2018-09-21 11:50:06 -0700

    basic mock up

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const memInfo3      = "<div class='team-member-3--info'><h1>Team Member 3</h1><p
 const memInfo4      = "<div class='team-member-4--info'><h1>Team Member 4</h1><p>Description of Team Member</p></div>";
 const memInfo5      = "<div class='team-member-5--info'><h1>Team Member 5</h1><p>Description of Team Member</p></div>";
 
+function ReplaceContentInContainer(id, content) {
+    const container = $(id);
+    container.empty();
+    container.html(content);
+}
+
 $(document).on('ready', function () {
     $('.slider').slick({
         variableWidth: true,
@@ -23,32 +29,26 @@ $(document).on('ready', function () {
     });
 
     $('.close').click(function () {
-        divTarget.html('');
-        divTarget.html(defaultText);
+        ReplaceContentInContainer(".team-info-target", defaultText);
     });
 
     $('.team-member-1-img').click(function () {
-        divTarget.html('');
-        divTarget.html(memInfo1);
+        ReplaceContentInContainer(".team-info-target", memInfo1);
     });
 
     $('.team-member-2-img').click(function () {
-        divTarget.html('');
-        divTarget.html(memInfo2);
+        ReplaceContentInContainer(".team-info-target", memInfo2);
     });
 
     $('.team-member-3-img').click(function () {
-        divTarget.html('');
-        divTarget.replaceWith(memInfo3);
+        ReplaceContentInContainer(".team-info-target", memInfo3);
     });
 
     $('.team-member-4-img').click(function () {
-        divTarget.html('');
-        divTarget.replaceWith(memInfo4);
+        ReplaceContentInContainer(".team-info-target", memInfo4);
     });
 
     $('.team-member-5-img').click(function () {
-        divTarget.html('');
-        divTarget.replaceWith(memInfo5);
+        ReplaceContentInContainer(".team-info-target", memInfo5);
     });
 });
