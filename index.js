c4d7e34206201eb52e7faa8e41eeb4a2faaab269
Roland Willis <rolandwillisiv@gmail.com>
2018-09-21 11:36:14 -0700

    wrestling with jquery to replace team member info

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-// $(document).on('ready', function () {
-//     $(".slider").slick({
-//         dots: true,
-//         infinite: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 3
-//     });
-// });
+const divTarget     = $(".team-info-target");
+
+const defaultText   = "<div class='default-info'><h1>Default Page</h1><p>Team Members</p></div>";
+const memInfo1      = "<div class='team-member-1--info'><h1>Team Member 1</h1><p>Description of Team Member</p></div>";
+const memInfo2      = "<div class='team-member-2--info'><h1>Team Member 2</h1><p>Description of Team Member</p></div>";
+const memInfo3      = "<div class='team-member-3--info'><h1>Team Member 3</h1><p>Description of Team Member</p></div>";
+const memInfo4      = "<div class='team-member-4--info'><h1>Team Member 4</h1><p>Description of Team Member</p></div>";
+const memInfo5      = "<div class='team-member-5--info'><h1>Team Member 5</h1><p>Description of Team Member</p></div>";
 
 $(document).on('ready', function () {
     $('.slider').slick({
         variableWidth: true,
         prevArrow: $('.prev'),
         nextArrow: $('.next')
-    })
+    });
 
     $('.left').click(function () {
         $('.slider').slick('slickPrev');
-    })
+    });
 
     $('.right').click(function () {
         $('.slider').slick('slickNext');
-    })
+    });
+
+    $('.close').click(function () {
+        divTarget.html('');
+        divTarget.html(defaultText);
+    });
+
+    $('.team-member-1-img').click(function () {
+        divTarget.html('');
+        divTarget.html(memInfo1);
+    });
+
+    $('.team-member-2-img').click(function () {
+        divTarget.html('');
+        divTarget.html(memInfo2);
+    });
+
+    $('.team-member-3-img').click(function () {
+        divTarget.html('');
+        divTarget.replaceWith(memInfo3);
+    });
+
+    $('.team-member-4-img').click(function () {
+        divTarget.html('');
+        divTarget.replaceWith(memInfo4);
+    });
+
+    $('.team-member-5-img').click(function () {
+        divTarget.html('');
+        divTarget.replaceWith(memInfo5);
+    });
 });
